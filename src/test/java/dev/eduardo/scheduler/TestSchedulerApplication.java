34140a41f6bcffbf0dd99a3e497a520a67b6a7de
Eduardo Tourinho <eduardo.tourinho@gmail.com>
2026-02-10 19:15:43 +0100

    chore: project setup

---
+++ src/test/java/dev/eduardo/scheduler/TestSchedulerApplication.java
@@ -0,0 +1,11 @@
+package dev.eduardo.scheduler;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SchedulerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
